06e3b177c5b849a05b02e545e2a205b6388fa9db
namefhf <2797822107@qq.com>
2023-11-18 16:28:44 +0800

    TS test

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -9,3 +9,10 @@ export function fn<T extends Lengthwise>(a: T): void {
   console.log(a.length);
 }
 const a = fn([1, 3, 4]);
+
+const obj = { a: 1, b: 2 };
+function getObjectValue<T extends object, K extends keyof T>(target: T, key: K): T[K] {
+  return target[key];
+}
+
+getObjectValue(obj, 'b');
