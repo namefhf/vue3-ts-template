ba11c4455bce6f4ac419b153a59fb4ad9e297d8e
namefhf <2797822107@qq.com>
2024-03-17 16:07:20 +0800

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,5 +28,6 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'off',
   },
 };
