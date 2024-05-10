74daa487c88cb317ddb0d501b291d7801decdfd2
namefhf <2797822107@qq.com>
2024-05-10 19:35:19 +0800

    fix type

--- src/components/floating-bubble/index.ts
+++ src/components/floating-bubble/index.ts
@@ -2,7 +2,7 @@ import FloatingBubble from './FloatingBubble';
 import type { App, Component } from 'vue';
 
 FloatingBubble.install = function (app: App) {
-  app.component(FloatingBubble.name, FloatingBubble);
+  app.component(FloatingBubble.name as string, FloatingBubble);
 };
 
 export default FloatingBubble as any;
