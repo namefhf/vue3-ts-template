afff86da56a0f810bb0c747915dc15340e340cd0
namefhf <2797822107@qq.com>
2024-08-10 20:56:52 +0800

    add .prettierignore

--- components.d.ts
+++ components.d.ts
@@ -7,11 +7,11 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AppProvider: (typeof import('./src/components/AppProvider.vue'))['default'];
-    AsyncComponent: (typeof import('./src/components/AsyncComponent.vue'))['default'];
-    Bubble: (typeof import('./src/components/Bubble.vue'))['default'];
-    DefineModel: (typeof import('./src/components/DefineModel.vue'))['default'];
-    RouterLink: (typeof import('vue-router'))['RouterLink'];
-    RouterView: (typeof import('vue-router'))['RouterView'];
+    AppProvider: typeof import('./src/components/AppProvider.vue')['default']
+    AsyncComponent: typeof import('./src/components/AsyncComponent.vue')['default']
+    Bubble: typeof import('./src/components/Bubble.vue')['default']
+    DefineModel: typeof import('./src/components/DefineModel.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
   }
 }
