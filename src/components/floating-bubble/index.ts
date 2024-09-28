42cdd917c9e5fb96261fe8e17a223a196261c778
namefhf <2797822107@qq.com>
2024-09-28 13:38:52 +0800

    delete useless files

--- src/components/floating-bubble/index.ts
+++
@@ -1,8 +0,0 @@
-import FloatingBubble from './FloatingBubble';
-import type { App, Component } from 'vue';
-
-FloatingBubble.install = function (app: App) {
-  app.component(FloatingBubble.name as string, FloatingBubble);
-};
-
-export default FloatingBubble as any;
