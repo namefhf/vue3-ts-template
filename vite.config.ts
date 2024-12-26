498b540698d19ef28dbeb32fa28ad91e1edd00e8
namefhf <2797822107@qq.com>
2024-12-26 15:55:56 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -100,9 +100,9 @@ export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
             vue: ['vue', 'vue-router', 'pinia'],
           },
           // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
-          entryFileNames: 'js/[name].[hash].js',
+          entryFileNames: 'js/entry.[name].[hash].js',
           // 用于命名代码拆分时创建的共享块的输出命名
-          chunkFileNames: 'js/[name].[hash].js',
+          chunkFileNames: 'js/chunk.[name].[hash].js',
           // 用于输出静态资源的命名，[ext]表示文件扩展名
           assetFileNames: '[ext]/[name].[hash].[ext]',
         },
