56648ad76f599dbc528c47831811d259694f8144
namefhf <2797822107@qq.com>
2025-03-08 10:48:08 +0800

    bump

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,23 @@
 import pluginJs from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import autoImport from './.eslintrc-auto-import.mjs';
 
-export default [
-  { files: ['**/*.{js,jsx,mjs,cjs,ts,tsx,vue}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node, ...autoImport.globals } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+export default defineConfig(
   {
+    name: 'my-eslint-config',
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx,vue}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node, ...autoImport.globals } },
+    extends: [pluginJs.configs.recommended, tseslint.configs.recommended, pluginVue.configs['flat/essential']],
     rules: {
-      'vue/multi-word-component-names': 'off',
+      'prefer-const': 'error',
+      'prefer-template': 'error',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      'prefer-template': 'error',
+      'no-unused-vars': 'off',
+      'vue/multi-word-component-names': 'off',
     },
   },
   {
@@ -30,24 +32,70 @@ export default [
       },
     },
   },
-  {
-    ignores: [
-      '**/snapshot*',
-      '**/dist',
-      '**/lib',
-      '**/es',
-      '**/esm',
-      '**/node_modules',
-      'src/_common',
-      '**/static',
-      '**/cypress',
-      'script/test/cypress',
-      '**/_site',
-      '**/temp*',
-      '**/static/',
-      '!**/.prettierrc',
-      '**/*.d.ts',
-      'src/vite-env.d.ts',
-    ],
-  },
-];
+  globalIgnores([
+    '**/snapshot*',
+    '**/dist',
+    '**/lib',
+    '**/es',
+    '**/esm',
+    '**/node_modules',
+    'src/_common',
+    '**/static',
+    '**/cypress',
+    'script/test/cypress',
+    '**/_site',
+    '**/temp*',
+    '**/static/',
+    '!**/.prettierrc',
+    '**/*.d.ts',
+    'src/vite-env.d.ts',
+  ]),
+);
+
+// export default [
+//   { files: ['**/*.{js,jsx,mjs,cjs,ts,tsx,vue}'] },
+//   { languageOptions: { globals: { ...globals.browser, ...globals.node, ...autoImport.globals } } },
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   ...pluginVue.configs['flat/essential'],
+//   {
+//     rules: {
+//       'vue/multi-word-component-names': 'off',
+//       '@typescript-eslint/no-unused-vars': 'off',
+//       '@typescript-eslint/no-explicit-any': 'warn',
+//       'prefer-template': 'error',
+//     },
+//   },
+//   {
+//     files: ['**/*.vue'],
+//     languageOptions: {
+//       parserOptions: {
+//         parser: tseslint.parser,
+//         allowImportExportEverywhere: true,
+//         ecmaFeatures: {
+//           jsx: true,
+//         },
+//       },
+//     },
+//   },
+//   {
+//     ignores: [
+//       '**/snapshot*',
+//       '**/dist',
+//       '**/lib',
+//       '**/es',
+//       '**/esm',
+//       '**/node_modules',
+//       'src/_common',
+//       '**/static',
+//       '**/cypress',
+//       'script/test/cypress',
+//       '**/_site',
+//       '**/temp*',
+//       '**/static/',
+//       '!**/.prettierrc',
+//       '**/*.d.ts',
+//       'src/vite-env.d.ts',
+//     ],
+//   },
+// ];
