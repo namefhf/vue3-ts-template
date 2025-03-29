6bbbe579f8c5f215013404644a0870560bbc5875
namefhf <2797822107@qq.com>
2025-03-29 13:41:38 +0800

    新增 useState 组合函数的选项支持，允许配置只读和浅层引用

--- src/composables/useState.ts
+++ src/composables/useState.ts
@@ -1,13 +1,19 @@
 import type { MaybeRefOrGetter, Ref, DeepReadonly } from 'vue';
-import { ref, toValue, readonly } from 'vue';
+import { ref, toValue, readonly as VueReadonly } from 'vue';
 
-function useState<T>(initialValue: MaybeRefOrGetter<T>): [DeepReadonly<Ref<T>>, (value: T) => void] {
-  const state = ref(toValue(initialValue)) as Ref<T>;
+interface Options {
+  readonly?: boolean;
+  shallow?: boolean;
+}
+
+function useState<T>(initialValue: MaybeRefOrGetter<T>, options?: Options): [DeepReadonly<Ref<T>>, (value: T) => void] {
+  const { readonly = true, shallow = false } = options || {};
+  const state = shallow ? shallowRef(toValue(initialValue)) : (ref(toValue(initialValue)) as Ref<T>);
 
   const setState = (value: T) => {
     state.value = value;
   };
 
-  return [readonly(state), setState];
+  return [readonly ? VueReadonly(state) : state, setState];
 }
 export default useState;
