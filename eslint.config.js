703a9a9384f05e4f81bf5ed6e7bc030f3d18f309
namefhf <2797822107@qq.com>
2025-08-04 18:46:19 +0800

    refactor: clean up eslint configuration by removing unused ignores and commented code

--- eslint.config.js
+++ eslint.config.js
@@ -40,7 +40,6 @@ export default defineConfig(
     '**/es',
     '**/esm',
     '**/node_modules',
-    'src/_common',
     '**/static',
     '**/cypress',
     'script/test/cypress',
@@ -52,51 +51,3 @@ export default defineConfig(
     'src/vite-env.d.ts',
   ]),
 );
-
-// export default [
-//   { files: ['**/*.{js,jsx,mjs,cjs,ts,tsx,vue}'] },
-//   { languageOptions: { globals: { ...globals.browser, ...globals.node, ...autoImport.globals } } },
-//   pluginJs.configs.recommended,
-//   ...tseslint.configs.recommended,
-//   ...pluginVue.configs['flat/essential'],
-//   {
-//     rules: {
-//       'vue/multi-word-component-names': 'off',
-//       '@typescript-eslint/no-unused-vars': 'off',
-//       '@typescript-eslint/no-explicit-any': 'warn',
-//       'prefer-template': 'error',
-//     },
-//   },
-//   {
-//     files: ['**/*.vue'],
-//     languageOptions: {
-//       parserOptions: {
-//         parser: tseslint.parser,
-//         allowImportExportEverywhere: true,
-//         ecmaFeatures: {
-//           jsx: true,
-//         },
-//       },
-//     },
-//   },
-//   {
-//     ignores: [
-//       '**/snapshot*',
-//       '**/dist',
-//       '**/lib',
-//       '**/es',
-//       '**/esm',
-//       '**/node_modules',
-//       'src/_common',
-//       '**/static',
-//       '**/cypress',
-//       'script/test/cypress',
-//       '**/_site',
-//       '**/temp*',
-//       '**/static/',
-//       '!**/.prettierrc',
-//       '**/*.d.ts',
-//       'src/vite-env.d.ts',
-//     ],
-//   },
-// ];
